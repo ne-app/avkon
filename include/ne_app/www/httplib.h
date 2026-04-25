59f5bd9748c067a453604e839b5957a6ccde2529
Amlal <amlal@ne-app.eu>
2026-04-25 07:42:59 +0200

    [CHORE] Update files source tree.

--- include/ne_app/www/httplib.h
+++ include/ne_app/www/httplib.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache 2.0
 // Copyright 2026, Ne.app, All rights reserved
-// Official repository: https://github.com/ne-app/dojo
+// Official repository: https://github.com/ne-app/avkon
 
 #pragma once
 
