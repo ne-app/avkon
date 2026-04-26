83eab569da9812ed451a0ebb8e88f816f63e85b1
Amlal <amlal@ne-app.eu>
2026-04-26 08:42:28 +0200

    [FEAT] Updated CMake, added pdf_store benchmark.
    Signed-off-by: Amlal <amlal@ne-app.eu>

---
+++ benchmark/pdf_store/pdf_store.bm.cpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Ne.app. All rights reserved
+// Official repository: https://github.com/ne-app/avkon
+
+#include <ne_app/core/pdf.hpp>
+
+static void BM_ReadPDF(benchmark::State& state) {
+
+}
