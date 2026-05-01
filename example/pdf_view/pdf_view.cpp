98f0115ab32e3553b085b05131cbf777d1c6b22f
Amlal <amlal@ne-app.eu>
2026-05-01 12:01:53 +0200

    [FEAT] src: Update example for pdf.

--- example/pdf_view/pdf_view.cpp
+++ example/pdf_view/pdf_view.cpp
@@ -6,5 +6,5 @@
 
 int main(int argc, char** argv) {
     if (argc < 2) return 1;
-    return ::pdf_view_document(argv[1], ::strlen(argv[1]));
+    return ::ne_app::pdf::render(argv[1], ::strlen(argv[1]));
 }
