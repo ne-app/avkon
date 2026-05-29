224204f2721fea23e0c1ccbcff257bd0e8b5b902
Amlal El Mahrouss <amlal@nekernel.org>
2026-05-29 14:51:59 +0200

    [CHORE] src: updated Avkon API.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ include/ne_app/core/core.hpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Ne.app. All rights reserved
+// Official repository: https://github.com/ne-app/avkon
+
+#pragma once
+
+#include <ne_app/core/pdf.hpp>
+#include <ne_app/core/store.hpp>
--- include/ne_app/www/webview.hpp
+++ include/ne_app/www/webview.hpp
@@ -10,6 +10,6 @@
 #ifndef WEBVIEW_ROOT_H
 #define WEBVIEW_ROOT_H
 
-#include "webview/webview.h"
+#include <ne_app/www/webview/webview.h>
 
 #endif // WEBVIEW_ROOT_H
