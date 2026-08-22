e6fc5aa522dffba53dfff0e8f4c9dd36657e4c23
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-22 08:07:41 +0200

    FIX: update error handling to use EXIT_FAILURE in HTML and PDF view examples

--- example/html_view/html_view.ex.cpp
+++ example/html_view/html_view.ex.cpp
@@ -5,11 +5,11 @@
 #include <ne_app/core/pdf.hpp>
 
 int main(int argc, char** argv) {
-  if (argc < 2) return 1;
+  if (argc < 2) return EXIT_FAILURE;
 
   try {
     return ::ne_app::html::render(argv[1], ::strlen(argv[1]));
   } catch (...) {
-    return 1;
+    return EXIT_FAILURE;
   }
 }
