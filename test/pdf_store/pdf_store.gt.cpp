b865276b9a7cbb6bf0dce51fdf2c0ba46c659644
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-31 21:30:57 +0200

    CHORE: update pdf store google test.

---
+++ test/pdf_store/pdf_store.gt.cpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Ne.app. All rights reserved
+// Official repository: https://github.com/ne-app/adb
+
+#include <ne_app/core/pdf.hpp>
+
+static void GT_ReadPDF(benchmark::State& state) {
+
+}
